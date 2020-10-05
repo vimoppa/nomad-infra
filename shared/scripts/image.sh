656dc03aaaeded56b0215501f3340204aa09cc1c
Josh <mail@chukwuanu.com>
2020-10-05 16:09:25 +0100

    update image setup script

--- shared/scripts/image.sh
+++ shared/scripts/image.sh
@@ -9,13 +9,12 @@ set -xe
 # Disable interactive apt prompts
 export DEBIAN_FRONTEND=noninteractive
 
-cd /tmp
+cd /tmp/
 
 function installDependencies() {
     echo "Installing dependencies..."
-    sudo apt-get install -y software-properties-common 2>/dev/null
     sudo apt-get update 2>/dev/null
-    sudo apt-get install -y unzip vim tree jq curl tmux 2>/dev/null
+    sudo apt-get install -y unzip vim tree jq curl tmux apt-transport-https ca-certificates software-properties-common 2>/dev/null
 }
 
 function installDocker() {
@@ -23,17 +22,13 @@ function installDocker() {
     sudo apt-get update
     echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
     sudo apt-get remove docker docker-engine docker.io 2>/dev/null
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) \
-      stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt-key fingerprint 0EBFCD88
     sudo apt-get update
     sudo apt-get install -y docker-ce
-    sudo usermod -aG docker ubuntu
     sudo service docker restart
+    # Make sure we can actually use docker as the ubuntu user
     sudo usermod -aG docker ubuntu
     sudo docker --version
 }
@@ -55,64 +50,62 @@ function installSSL() {
 }
 
 function installNomad() {
-    nomad_download=https://releases.hashicorp.com/nomad/${nomad_version}/nomad_${nomad_version}_linux_amd64.zip
-    nomad_bin_dir=/usr/local/bin/nomad
-
     echo "Fetching Nomad..."
-    curl -sLo nomad.zip ${nomad_download}
+    curl -sLo nomad.zip https://releases.hashicorp.com/nomad/${nomad_version}/nomad_${nomad_version}_linux_amd64.zip
 
     echo "Installing Nomad..."
     unzip nomad.zip 2>/dev/null
-    sudo chmod +x nomad
     sudo chown root:root nomad
-    sudo install nomad ${nomad_bin_dir}
+    sudo mv nomad /usr/local/bin/nomad
+    nomad version
     nomad -autocomplete-install
 }
 
 function installConsul() {
-    consul_download=https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip
-    consul_bin_dir=/usr/local/bin/consul
-
     echo "Fetching Consul..."
-    curl -sLo consul.zip ${consul_download}
+    curl -sLo consul.zip https://releases.hashicorp.com/consul/${consul_version}/consul_${consul_version}_linux_amd64.zip
 
     echo "Installing Consul..."
     unzip consul.zip 2>/dev/null
-    sudo chmod +x consul
     sudo chown root:root consul
-    sudo mv consul ${consul_bin_dir}
+    sudo mv consul /usr/local/bin/consul
+    consul version
 }
 
 function installConsulTemplate() {
-    consul_template_download=https://releases.hashicorp.com/consul-template/${consul_template_version}/consul-template_${consul_template_version}_linux_amd64.zip
-    consul_template_bin_dir=/usr/local/bin/consul-template
-
     echo "Fetching Consul Template..."
-    curl -sLo consul-template.zip ${consul_template_download}
+    curl -sLo consul-template.zip https://releases.hashicorp.com/consul-template/${consul_template_version}/consul-template_${consul_template_version}_linux_amd64.zip
 
     echo "Installing Consul Template..."
     unzip consul-template.zip 2>/dev/null
     sudo chmod +x consul-template
     sudo chown root:root consul-template
-    sudo mv consul-template ${consul_template_bin_dir}
+    sudo mv consul-template /usr/local/bin/consul-template
+    consul-template -version
 }
 
 function installVault() {
-    vault_download=https://releases.hashicorp.com/vault/${vault_version}/vault_${vault_version}_linux_amd64.zip
-    vault_bin_dir=/usr/local/bin/vault
-
     echo "Fetching Vault..."
-    curl -sLo vault.zip ${vault_download}
+    curl -sLo vault.zip https://releases.hashicorp.com/vault/${vault_version}/vault_${vault_version}_linux_amd64.zip
 
     echo "Installing Vault..."
     unzip vault.zip 2>/dev/null
-    sudo chmod +x vault
     sudo chown root:root vault
-    sudo mv vault ${vault_bin_dir}
+    sudo mv vault /usr/local/bin/vault
+    vault version
 }
 
 function installFabio() {
     fabio_download=https://github.com/fabiolb/fabio/releases/download/v${fabio_version}/fabio-${fabio_version}-go1.15-linux_amd64
+}
+
+function commonConfig() {
+    for bin in nomad vault consul; do
+        complete -C /usr/local/bin/$bin $bin
+        sudo mkdir --parents /opt/$bin
+        sudo mkdir --parents /etc/$bin.d
+        sudo chmod 0755 /etc/$bin.d
+    done
 }
 
 # Install software
@@ -133,10 +126,10 @@ installSSL
 # Install Consul
 installConsul
 
-# Install Consul Template
-installConsulTemplate
+# # Install Consul Template
+# installConsulTemplate
 
-# Install Fabio
+# # Install Fabio
 # installFabio
 
 # Install Vault
@@ -144,3 +137,6 @@ installVault
 
 # Install Nomad
 installNomad
+
+# common configuration used on both client and server
+commonConfig
